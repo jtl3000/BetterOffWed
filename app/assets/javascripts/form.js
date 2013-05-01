cc2a8118a3822e8b5a2b80fe6de19016ed8be6d1
Jon Levine <jonlevine@gmail.com>
2013-04-30 20:04:14 -0400

    Javascript changes and Active Admin CSS

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -6,36 +6,47 @@
 //  })
 
 
+//$(function () {
+
+//$('#Postponement').change(function () {
+//    if ($("#NewWeddingDate").is(":hidden")) {
+//     $("#NewWeddingDate").show("slow");
+//    } else {
+//      $("#NewWeddingDate").slideUp();
+//   }
+//  })
+//})
+
+//$(function () {
+
+//$('#WillDivorce').change(function () {
+//    if ($("#DivorceDate").is(":hidden")) {
+//      $("#DivorceDate").show("slow");
+//    } else {
+//      $("#DivorceDate").slideUp();
+//    }
+//  })
+//})
+
+//$(function () {
+
+//$('#OneDead').change(function () {
+//    if ($("#WhoKillsWho").is(":hidden")) {
+//      $("#WhoKillsWho").show("slow");
+//    } else {
+//      $("#WhoKillsWho").slideUp();
+//    }
+//  })
+//})
+
 $(function () {
-
-$('#Postponement').change(function () {
-    if ($("#NewWeddingDate").is(":hidden")) {
-      $("#NewWeddingDate").show("slow");
+var $Postponement = $('Postponement'),
+    $NewWeddingDate = $('NewWeddingDate');
+$Postponement.change(function() {
+    if ($Postponement.val() == 'No') {
+        $NewWeddingDate.removeAttr('disabled');
     } else {
-      $("#NewWeddingDate").slideUp();
+        $NewWeddingDate.attr('disabled', 'disabled').val('');
     }
-  })
+}).trigger('change'); // added trigger to calculate initial state
 })
-
-$(function () {
-
-$('#WillDivorce').change(function () {
-    if ($("#DivorceDate").is(":hidden")) {
-      $("#DivorceDate").show("slow");
-    } else {
-      $("#DivorceDate").slideUp();
-    }
-  })
-})
-
-$(function () {
-
-$('#OneDead').change(function () {
-    if ($("#WhoKillsWho").is(":hidden")) {
-      $("#WhoKillsWho").show("slow");
-    } else {
-      $("#WhoKillsWho").slideUp();
-    }
-  })
-})
-
