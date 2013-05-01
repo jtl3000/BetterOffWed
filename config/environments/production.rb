c7ee9bf189a8d431d51a7f603cf89ed638187c23
Jon Levine <jonlevine@gmail.com>
2013-05-01 14:34:29 -0400

    added active admin directory and precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Betteroffwed::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w[active_admin.css active_admin.js]
+  config.assets.precompile += %w[active_admin.css active_admin/print.css active_admin.js]
 
 
   # Disable delivery errors, bad email addresses will be ignored
